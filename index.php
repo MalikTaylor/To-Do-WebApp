8fa703e233b060528e2f02deb73ed8b034628330
MalikTaylor <67035475+MalikTaylor@users.noreply.github.com>
2022-06-02 03:46:22 -0500

    fill in notch on Iphone

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name='viewport' content='initial-scale=1, viewport-fit=cover'>
+        <meta name="theme-color" content="#202124">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
